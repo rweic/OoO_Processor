d240729fa3718ca0ca6188038947a179237c0f1e
Elijah Thomas Carino <ecarino@uw.edu>
2023-05-09 20:50:50 -0700

    72% line coverage, also added coverage commmand to makefile

--- in-order/src/icache.v
+++ in-order/src/icache.v
@@ -1,4 +1,4 @@
-module icache #(parameter MEM_DEP = 64) (
+module icache #(parameter MEM_DEP = 192) (
     input logic [31:0] addr,     // Address input (32 bits)
     output logic [31:0] data     // Data output (32 bits)
 );
@@ -56,7 +56,7 @@ module icache #(parameter MEM_DEP = 64) (
         for(i = 0; i < MEM_DEP; i = i + 1) begin
             instr_mem[i] = 8'b0;
         end
-        $readmemh("../../in-order/src/icache_init.txt", instr_mem, 0, 55);
+        $readmemh("../../in-order/src/icache_init.txt", instr_mem, 0, 170);
     end
 
 endmodule
